af0a2c7fe9a7a5e0910cf8a8a590116aa0b69a1f
Ahmad Ghadban <ahmadghadban02@gmail.com>
2023-06-08 20:22:04 -0400

    added a navbar

--- 2km-site/src/app/app.module.ts
+++ 2km-site/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './products/product-card/product-card.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductCardComponent,
+    ProductListComponent,
+    NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
