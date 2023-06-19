e466347e3068d2c8e46552fd75d0a07b25644988
Ahmad Ghadban <ahmadghadban02@gmail.com>
2023-06-18 20:09:59 -0400

    added a service

--- 2km-site/src/app/app.module.ts
+++ 2km-site/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './products/product-card/product-card.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SellingService } from './services/selling.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductCardComponent,
     ProductListComponent,
-    NavBarComponent
-   ],
-  imports: [
-    BrowserModule
+    NavBarComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule],
+  providers: [SellingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- 2km-site/src/app/products/product-list/product-list.component.ts
+++ 2km-site/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SellingService } from 'src/app/services/selling.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,46 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  Products: Array<any> = [
-    {
-      Id: 1,
-      Code: '2KM0001',
-      Size: 'small',
-      Price: 120,
-    },
-    {
-      Id: 2,
-      Code: '2KM0002',
-      Size: 'small',
-      Price: 130,
-    },
-    {
-      Id: 3,
-      Code: '2KM0003',
-      Size: 'small',
-      Price: 140,
-    },
-    {
-      Id: 4,
-      Code: '2KM0004',
-      Size: 'small',
-      Price: 140,
-    },
-    {
-      Id: 5,
-      Code: '2KM0005',
-      Size: 'small',
-      Price: 150,
-    },
-    {
-      Id: 6,
-      Code: '2KM0006',
-      Size: 'small',
-      Price: 180,
-    },
-  ];
+  Products!: any;
 
-  constructor() {}
+  constructor(private sellingService: SellingService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.sellingService.getAllProducts().subscribe(
+      (data) => {
+        this.Products = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
