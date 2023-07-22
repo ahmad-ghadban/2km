fb6183552a1e0be83013f10623f2114dae377e03
Ahmad Ghadban <ahmadghadban02@gmail.com>
2023-07-22 04:27:39 -0400

    added data to product detail and removed excess buttons from card

--- 2km-site/src/app/products/product-detail/product-detail.component.ts
+++ 2km-site/src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SellingService } from 'src/app/services/selling.service';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,9 +10,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   public productId: number;
-  constructor(private route: ActivatedRoute) {}
+  product: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private sellingService: SellingService
+  ) {}
 
   ngOnInit() {
     this.productId = this.route.snapshot.params['id'];
+
+    this.route.params.subscribe((params) => {
+      this.productId = +params['id'];
+      this.sellingService.getAllProducts().subscribe(
+        (data: any) => {
+          this.product = data[this.productId - 1];
+          if (this.product === undefined) {
+            this.router.navigate(['/']);
+          }
+          console.error(this.product);
+        },
+        (error) => this.router.navigate(['/'])
+      );
+    });
   }
 }
